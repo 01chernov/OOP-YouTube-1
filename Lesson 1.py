481d827bb8fe01fa8943135d35b35d682d564d71
Andrei <01chernov@mail.ru>
2018-04-30 19:14:55 +0300

    Initial commit2

--- Lesson 1.py
+++ Lesson 1.py
@@ -1,1 +1,16 @@
-print('Hello, Dasha!')
+print('Hello, Dasha!') 
+num1 = 20
+num2 = num1
+
+corteg1 = 1, 2, 3, 4
+corteg2 = (10, 20, 30, 40)
+
+num3 = int(input('Введите третье число: '))
+word = 'слово'
+
+print num1
+print num3
+print (num2 + num3)
+print ('\nКортеж первый:', corteg1)
+print('\nКортеж второй:', corteg2)
+print('Второе число из второго кортежа:', corteg2[1])
